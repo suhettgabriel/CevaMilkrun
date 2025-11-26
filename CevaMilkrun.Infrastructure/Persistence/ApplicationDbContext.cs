62b3fe8484123ace9fadba671e46ab46247feee7
suhettgabriel <suhettgabriel1983@gmail.com>
2025-11-26 14:01:49 -0300

    feat(domain): implement core entities (MilkrunTrip, SearchHistory) and interfaces with nullability handling

---
+++ CevaMilkrun.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CevaMilkrun.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CevaMilkrun.Infrastructure.Persistence
+{
+    // A classe deve herdar de DbContext
+    public class ApplicationDbContext : DbContext
+    {
+        // O construtor deve repassar as opções para a base
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<SearchHistory> SearchHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Boa prática chamar o base
+
+            modelBuilder.Entity<SearchHistory>(e =>
+            {
+                e.HasKey(x => x.Id);
+                e.Property(x => x.PhoneNumber).HasMaxLength(20);
+            });
+        }
+    }
+}
